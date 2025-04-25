09a1251e64de3de4b55b49b349d252325caa9a60
T-venkata <iamvenkata45@gmail.com>
2025-04-26 00:20:17 +0530

    Update backend.tf

--- eks-terraform/backend.tf
+++ eks-terraform/backend.tf
@@ -7,8 +7,8 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "testdevprdtest"
-    key    = "k8/terraform.tfstate"
+    bucket = "venkatdeveks"
+    key    = "eks/terraform.tfstate"
     region = "us-east-1"
   }
 
